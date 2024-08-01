6f8532b84810ee23d44beedfd1330d44a65b8892
break-and-enter <clubdedov@gmail.com>
2024-08-01 02:09:45 +0200

    first commit - add Babaika model

---
+++ babaika/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Babaika
+
+admin.site.register(Babaika)
---
+++ babaika/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Babaika(models.Model):
+    name = models.CharField(max_length=50)
