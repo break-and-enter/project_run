6868166955e921395da3f08814ebe47ace65e86b
break-and-enter <clubdedov@gmail.com>
2024-08-03 01:08:23 +0200

    change views

--- babaika/views.py
+++ babaika/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+from babaika.models import Auto
+
+
 # Create your views here.
 def index_auto(request):
-    return JsonResponse({'s':4})
+    qs = Auto.obhects.all()
+    return JsonResponse(qs)
