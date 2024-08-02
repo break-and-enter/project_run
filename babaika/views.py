499380c1b455e4063277e886d298ad4676d369ed
break-and-enter <clubdedov@gmail.com>
2024-08-03 01:54:00 +0200

    change views 4

--- babaika/views.py
+++ babaika/views.py
@@ -6,7 +6,7 @@ from babaika.models import Auto
 def index_auto(request):
     qs = Auto.objects.all()
     print(qs)
-    d = {}
+    d = []
     for i in qs:
-        d[i.id] = i.name
-    return JsonResponse(d)
+        d.append(f'name:{i.name}')
+    return JsonResponse(d, safe=False)
