7f50ad468bbf9c1d8c58a6651fd05fc3000167bc
break-and-enter <clubdedov@gmail.com>
2024-08-03 00:57:35 +0200

    add admin.py, change model

--- babaika/admin.py
+++ babaika/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Babaika
+from .models import Babaika, Auto
 
 admin.site.register(Babaika)
+admin.site.register(Auto)
--- babaika/models.py
+++ babaika/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Babaika(models.Model):
     name = models.CharField(max_length=50)
 
+
 class Auto(models.Model):
-    m = models.IntegerField(default=0)
+    name = models.CharField(max_length=50, default='')
