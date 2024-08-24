343a96f4126eca4cec6b448828cf685005617551
break-and-enter <clubdedov@gmail.com>
2024-08-25 00:52:52 +0200

    create serializers.py , add ViewSet and router

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Run
+from .serializers import RunSerializer
 
-# Create your views here.
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
