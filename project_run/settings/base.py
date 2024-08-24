684c1e0a04b09a86d9d404556e093a82f0e2c837
break-and-enter <clubdedov@gmail.com>
2024-08-24 19:30:31 +0200

    create model Run, install DRF

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_run'
+    'app_run',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
