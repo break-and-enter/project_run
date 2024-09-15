20682370486f416a6e0a1c02c4d7c96709b0f6cb
break-and-enter <clubdedov@gmail.com>
2024-09-15 15:43:32 +0200

    forget comma in INSTALLED_APPS list

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    'rest_framework',
 
-    'app_run',
+    'app_run'
 ]
 
 MIDDLEWARE = [
