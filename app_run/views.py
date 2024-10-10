f0c3ff56d1d1b01ca4a7f6151a308ee4b2a30f1b
break-and-enter <clubdedov@gmail.com>
2024-10-10 07:30:07 +0200

    delete unused harvesine from requirements.txt

--- app_run/views.py
+++ app_run/views.py
@@ -5,7 +5,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Run, Position
 from .serializers import RunSerializer, PositionSerializer
-from haversine import haversine
 from geopy.distance import geodesic
 
 
