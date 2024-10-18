0b1162be2aedd096f6d517bd9a6b0deb58fcff3a
break-and-enter <clubdedov@gmail.com>
2024-10-18 23:52:48 +0200

    add speed to Run and distance to Positions

--- app_run/models.py
+++ app_run/models.py
@@ -14,6 +14,7 @@ class Run(models.Model):
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='init')
     distance = models.FloatField(default=0)
+    speed = models.FloatField(default=0)
     run_time_seconds = models.IntegerField(default=0)
 
 class Position(models.Model):
@@ -21,6 +22,7 @@ class Position(models.Model):
     longitude = models.DecimalField(decimal_places=4, max_digits=8)
     date_time = models.DateTimeField()
     speed = models.FloatField(default=0)
+    distance = models.FloatField(default=0)
     run = models.ForeignKey(Run, on_delete=models.CASCADE)
 
 # @property
