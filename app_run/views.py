d44e1a13542c7b3bb121321ed7734565c3a9fb3f
break-and-enter <clubdedov@gmail.com>
2024-11-19 20:51:52 +0100

    add 404 status 4

--- app_run/views.py
+++ app_run/views.py
@@ -209,9 +209,11 @@ class CoachRatingView(APIView):
         if not 1<= int(rating) <=5:
             return Response({'message': f'rating не в пределах от 1 до 5. Ваше значение {rating}'}, status=status.HTTP_400_BAD_REQUEST)
 
-        if not User.objects.filter(id=coach_id).exists():
+        if not User.objects.filter(id=coach_id, is_staff=True).exists():
             return Response({'message': f'Тренера с id {coach_id} не существует'},
                             status=status.HTTP_404_NOT_FOUND)
+
+
 
         if Subscription.objects.filter(coach=coach_id, athlete=athlete_id).exists():
             subscription = Subscription.objects.get(coach=coach_id, athlete=athlete_id)
