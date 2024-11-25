4df146cc26fe0099bbe94267d11906d0b9ca19e0
break-and-enter <clubdedov@gmail.com>
2024-11-25 12:19:35 +0100

    put all annotations to one query

--- app_run/views.py
+++ app_run/views.py
@@ -228,18 +228,24 @@ class AnalyticsCoachView(APIView):
     def get(self, request, coach_id):
         coach_queryset = Subscription.objects.filter(coach=coach_id)
 
-        qs_with_max_distance_field = coach_queryset.annotate(max_distance=Max('athlete__run__distance'))
-        longest_qs = qs_with_max_distance_field.order_by('-max_distance').first()
+        qs_with_additional_fields = coach_queryset.annotate(
+            max_distance=Max('athlete__run__distance'),
+            sum_distances=Sum('athlete__run__distance'),
+            avg_speed=Avg('athlete__run__speed')
+        )
+
+        # qs_with_max_distance_field = coach_queryset.annotate(max_distance=Max('athlete__run__distance'))
+        longest_qs = qs_with_additional_fields.order_by('-max_distance').first()
         longest_run_value = longest_qs.max_distance
         longest_run_user = longest_qs.athlete.id
 
-        qs_with_sum_distances_field = coach_queryset.annotate(sum_distances=Sum('athlete__run__distance'))
-        max_total_run_qs = qs_with_sum_distances_field.order_by('-sum_distances').first()
+        # qs_with_sum_distances_field = coach_queryset.annotate(sum_distances=Sum('athlete__run__distance'))
+        max_total_run_qs = qs_with_additional_fields.order_by('-sum_distances').first()
         total_run_value = max_total_run_qs.sum_distances
         total_run_user = max_total_run_qs.athlete.id
 
-        qs_with_avg_speed_field = coach_queryset.annotate(avg_speed=Avg('athlete__run__speed'))
-        max_avg_speed_qs = qs_with_avg_speed_field.order_by('-avg_speed').first()
+        # qs_with_avg_speed_field = coach_queryset.annotate(avg_speed=Avg('athlete__run__speed'))
+        max_avg_speed_qs = qs_with_additional_fields.order_by('-avg_speed').first()
         speed_avg_value = max_avg_speed_qs.avg_speed
         speed_avg_user = max_avg_speed_qs.athlete.id
 
